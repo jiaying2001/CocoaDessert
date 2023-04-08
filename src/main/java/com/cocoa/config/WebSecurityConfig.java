2ea9c385eaa526a3101f2ec9346e347fc909febe
Jiaying Huang <3395592153@qq.com>
2023-04-08 14:09:35 +0100

    测试和编写md文件

--- src/main/java/com/cocoa/config/WebSecurityConfig.java
+++ src/main/java/com/cocoa/config/WebSecurityConfig.java
@@ -3,8 +3,11 @@ package com.cocoa.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -24,8 +27,15 @@ public class WebSecurityConfig {
 		 	http.formLogin((form -> form
 					.loginPage("/login")
 					.permitAll()))
-		 	.authorizeRequests()
-		 	.anyRequest().authenticated();
+		 	.authorizeHttpRequests((authorize) -> authorize.anyRequest().authenticated())
+		 	.csrf((csrf) -> csrf.disable())
+		 	.httpBasic(Customizer.withDefaults())
+			.oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt)
+			.sessionManagement((session) -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+			.exceptionHandling((exceptions) -> exceptions
+								.authenticationEntryPoint(new BearerTokenAuthenticationEntryPoint()))
+			.accessDeniedHandler(new BearerTokenAccessDeniedHandler())
+		 	;
 //		 	.antMatchers("/home/content").permitAll()
 //		 	.anyRequest().denyAll();
 		 	return http.build();
